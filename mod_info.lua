3e139222b47542866af39d402794b03eb1fcc6cc
Azraeel <45247351+Azraeel@users.noreply.github.com>
2020-08-30 17:11:19 -0500

    Update Version Number

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "ABM"
-uid = "8043c0e7-f9e7-483c-0004-a9ab1ec6445e"
-version = 4
+uid = "8043c0e7-f9e7-483c-0005-a9ab1ec6445e"
+version = 5
 description = "This is ABM (Azraeel's Balance Mod) This mod is intended to address not just unit balance but address economic balance. The major changes are the rebalancing of Energy Economices by making Energy more needed throughout the game, also rebalancing T3 Air Phase, and more! (I'll Add Changelog later)"
 author = "Azraeel and Others"
 icon = "/mods/Azraeel's Balance Mod/icon.png"
